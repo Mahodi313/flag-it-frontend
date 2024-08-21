bedc16022102ca9db8660d9ca49583a5b84c71a2
Adirsflo <rida.abdal@gmail.com>
2024-08-21 17:08:22 +0200

    testing branch merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import React from "react";
+
+// Router
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home.jsx";
+
+// Components & Hooks
+
+// CSS
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Tjena</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
