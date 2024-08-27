ea7e345510ada75599ae6e11de40ad93615f8b42
Adirsflo <rida.abdal@gmail.com>
2024-08-27 20:13:59 +0200

    landingpage almost completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 
+// Router Link
+import { Link } from "react-router-dom";
+
 import "./Home.css";
 
 function Home() {
   return (
     <>
-      <div>
-        <div>
+      <div id="landing-container-top">
+        <div id="landing-left">
           <h1>FLAGIT</h1>
           <h3>Put a flag on it!</h3>
-          <button>Gå till Quiz</button>
+          <Link to="/quizstart">
+            <button className="primary-btn">Gå till Quiz</button>
+          </Link>
         </div>
-        <ul>
-          <li>Information om hur man använder sidan</li>
-          <li>Quiz för att utmana dig själv och andra på hemsidan</li>
-          <li>Se din historik och utveckla din kunskap</li>
-        </ul>
+        <div id="landing-right">
+          <ul>
+            <li>Information om hur man använder sidan</li>
+            <li>Quiz för att utmana dig själv och andra på hemsidan</li>
+            <li>Se din historik och utveckla din kunskap</li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <div>
-          <p>01</p>
+      <div id="landing-container-bottom">
+        <div className="landing-box">
+          <h2>01</h2>
           <p>Säljande punkt</p>
         </div>
-        <div>
-          <p>02</p>
+        <div className="landing-box">
+          <h2>02</h2>
           <p>Säljande punkt</p>
         </div>
-        <div>
-          <p>03</p>
+        <div id="landing-box-3" className="landing-box">
+          <h2>03</h2>
           <p>Säljande punkt</p>
         </div>
       </div>
